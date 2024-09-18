355acd4971a7b72d4e1a70e26e4abdaa5f7a5ee9
ShaunFChen <shaunfchen@gmail.com>
2024-09-17 20:21:24 -0700

    Initial commit from cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project analyzes blood biomarkers to identify those associated with the development of diabetes and builds a machine learning model to predict diabetes risk. The work involves data cleaning, statistical analysis, and model evaluation to provide insights into diabetes progression and prediction.',
+    author='Shaun Chen',
+    license='MIT',
+)
