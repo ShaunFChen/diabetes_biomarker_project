569f83990985dcd203ae50f9b310c89d8edb0c9c
ShaunFChen <shaunfchen@gmail.com>
2024-09-21 16:29:56 -0700

    add unit tests.

---
+++ tests/test_example.py
@@ -0,0 +1,8 @@
+import pytest
+from src.data.data_utils import load_data
+
+
+def test_process_data():
+    sample_input = [1, 2, 3, 4]
+    expected_output = [2, 4, 6, 8]
+    assert expected_output == expected_output
