45719bd5c77008c8d2c4e3e5a05f8dbc158ddc52
ShaunFChen <shaunfchen@gmail.com>
2024-09-24 14:58:53 -0700

    save progress.

--- tests/test_example.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from src.data_utils import load_data
-
-
-def test_process_data():
-    sample_input = [1, 2, 3, 4]
-    expected_output = [2, 4, 6, 8]
-    assert expected_output == expected_output
